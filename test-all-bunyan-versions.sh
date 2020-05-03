4b76814a42e0ef13e4cbda2241e9ddc6e9bb74fd
Matthew Adams <matthew.adams@northscaler.com>
2020-05-03 09:03:51 -0500

    use latest in local tests, fail on error when testing all

--- test-all-bunyan-versions.sh
+++ test-all-bunyan-versions.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -e
 
 for v in $(npm view bunyan --json | fx 'require("semver-sort").asc(this.versions.filter(it => parseInt(it.match(/^(\d+)\.\d+\.\d+$/)[1]) >= 1)).join(" ")'); do
   echo "### testing bunyaner@$v"
